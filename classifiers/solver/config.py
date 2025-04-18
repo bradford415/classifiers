a9333139fbf7ba3c7a591679fa04c75330912723
Bradley Selee <bselee415@gmail.com>
2025-04-18 15:27:39 -0600

    Working on vit b16 config

--- classifiers/solver/config.py
+++ classifiers/solver/config.py
@@ -3,35 +3,32 @@ import ml_collections
 __all__ = ["resnet50_imagenet_config"]
 
 
-def get_testing():
-    """Returns a minimal configuration for testing."""
-    config = ml_collections.ConfigDict()
-    config.patches = ml_collections.ConfigDict({"size": (16, 16)})
-    config.hidden_size = 1
-    config.transformer = ml_collections.ConfigDict()
-    config.transformer.mlp_dim = 1
-    config.transformer.num_heads = 1
-    config.transformer.num_layers = 1
-    config.transformer.attention_dropout_rate = 0.0
-    config.transformer.dropout_rate = 0.1
-    config.classifier = "token"
-    config.representation_size = None
-    return config
+def vit_b16_imagenet_config():
+    """Returns the ViT-B/16 configuration.
+    
+    Naming convention: vit-{variant}{patch_size}_{dataset}_config
+    
+    Solver parameters derived from https://arxiv.org/pdf/2010.11929 in Table 3
 
-
-def vit_b16_config():
-    """Returns the ViT-B/16 configuration."""
+    ## TODO: make note to reference the DeiT paper and the small weight decay
+    """
     config = ml_collections.ConfigDict()
-    config.patches = ml_collections.ConfigDict({"size": (16, 16)})
-    config.hidden_size = 768
-    config.transformer = ml_collections.ConfigDict()
-    config.transformer.mlp_dim = 3072
-    config.transformer.num_heads = 12
-    config.transformer.num_layers = 12
-    config.transformer.attention_dropout_rate = 0.0
-    config.transformer.dropout_rate = 0.1
-    config.classifier = "token"
-    config.representation_size = None
+
+    # Optimizer params
+    config.optimizer = ml_collections.ConfigDict()
+    config.optimizer.name = "sgd"
+    config.optimizer.lr = 0.1
+    config.optimizer.weight_decay = 5e-2  # 0.05
+    config.optimizer.momentum = 0.9
+
+    # Scheduler params
+    config.step_lr_on = "epochs" # step the lr after n "epochs" or "steps" 
+    config.lr_scheduler = ml_collections.ConfigDict()
+    config.lr_scheduler.name = "reduce_lr_on_plateau"
+    config.lr_scheduler.mode = "min"
+    config.lr_scheduler.factor = 0.1
+    config.lr_scheduler.patience = 5  # epochs of no improvement
+
     return config
 
 
@@ -41,11 +38,6 @@ def resnet50_imagenet_config():
     Parameters defined in the ResNet paper https://arxiv.org/abs/1512.03385 section 3.4
     """
     config = ml_collections.ConfigDict()
-
-    # TODO: decide if I want this in the config.py or yaml General params; currently these params are not used
-    config.train_batch_size = 512
-    config.val_batch_size = 128
-    config.num_epochs = 90
 
     # Optimizer params
     config.optimizer = ml_collections.ConfigDict()
@@ -64,6 +56,21 @@ def resnet50_imagenet_config():
 
     return config
 
+
+def get_testing():
+    """Returns a minimal configuration for testing."""
+    config = ml_collections.ConfigDict()
+    config.patches = ml_collections.ConfigDict({"size": (16, 16)})
+    config.hidden_size = 1
+    config.transformer = ml_collections.ConfigDict()
+    config.transformer.mlp_dim = 1
+    config.transformer.num_heads = 1
+    config.transformer.num_layers = 1
+    config.transformer.attention_dropout_rate = 0.0
+    config.transformer.dropout_rate = 0.1
+    config.classifier = "token"
+    config.representation_size = None
+    return config
 
 # def get_l32_config():
 #     """Returns the ViT-L/32 configuration."""
